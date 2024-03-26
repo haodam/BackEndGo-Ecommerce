4840de7fcf97ae9e7b20c294b2294bf994102e78
dam hao <damanhhao3004@gmail.com>
2024-03-26 23:23:24 +0700

    update: gen code sqlc function users

--- internal/users/repository/query/user.sql
+++ internal/users/repository/query/user.sql
@@ -19,7 +19,6 @@ SET
     password_changed_at = COALESCE(sqlc.narg(password_changed_at), password_changed_at),
     full_name = COALESCE(sqlc.narg(full_name), full_name),
     email = COALESCE(sqlc.narg(email), email),
-
 WHERE
     username = sqlc.arg(username)
-    RETURNING *;
+RETURNING *;
